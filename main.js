180b6b3efbe09052a0ad4c442f82343c6c77e54a
starsweet0831 <star_sweet25@yahoo.com.tw>
2021-02-03 21:33:30 +0800

    新增連結

--- main.js
+++ main.js
@@ -6,5 +6,24 @@ $("#banner").slick({
 
 $("#works").slick({
     slidesToShow: 4,
-    slidesToScroll: 2
+    slidesToScroll: 2,
+// RWD
+    responsive: [
+        {
+            // 平板小於1025
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            // 手機小於767
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
